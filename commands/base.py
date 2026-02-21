d09540afcfeff0a0c17064cb1cf08e51c1ff1841
IntelBoy <maximbulavintsev2008@gmail.com>
2026-02-21 22:03:52 +1000

    - update 0.0.1  + base commands (start, help)

---
+++ commands/base.py
@@ -0,0 +1,36 @@
+
+class BaseCommands:
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
+
+    def register_commands(self):
+        self.bot.command_handler(['start'])(self.start_command)
+
+    def start_command(self, message):
+        user_id = message.from_user.id
+        if not self.db.users.find_one({"uid": user_id}):
+            self.db.users.insert_one({"uid": user_id, "moderator": False})
+        self.bot.reply_to(message, "Welcome to the ReWiki Bot! Use /help to see available commands.")
+
+    def help_command(self, message):
+        user_id = message.from_user.id
+        user = self.db.users.find_one({"uid": user_id})
+
+        help_text = (
+            "Available commands:\n"
+            "/start - Start the bot and register yourself\n"
+            "/help - Show this help message\n"
+            "/save <text> - Save an article\n"
+            "/remove <text> - Remove an article from your saved list\n"
+            "/list - List your saved articles\n"
+        )
+
+        if user["moderator"]:
+            help_text = (
+                "/create <name> <content> - Create a new article\n"
+                "/edit <name> <content> - Edit an existing article\n"
+                "/delete <name> - Delete an article\n"
+            )
+
+        self.bot.reply_to(message, help_text)
---
+++ database.py
@@ -0,0 +1,33 @@
+# database.py
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from config import MONGO_DB_URI
+
+
+class DatabaseManager:
+    def __init__(self):
+        self.client = MongoClient(MONGO_DB_URI, server_api=ServerApi('1'))
+        self.db = self.client['rewikibot']
+        self._init_collections()
+        self._init_indexes()
+
+    def _init_collections(self):
+        if 'users' not in self.db.list_collection_names():
+            self.db.create_collection('users')
+
+        if 'articles' not in self.db.list_collection_names():
+            self.db.create_collection('articles')
+
+
+    def _init_indexes(self):
+        self.db.users.create_index('chat_id', unique=True)
+        self.db.users.create_index('username')
+        self.db.articles.create_index('chat_id')
+        self.db.articles.create_index('timestamp')
+
+    def close(self):
+        self.client.close()
+
+db_manager = DatabaseManager()
+db = db_manager.db
